609697cb0baefb190df4ec7e15030cc066e52a76
Matej <Stano>
2020-12-08 02:30:08 +0100

    Firebase logIn subscription fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React, { FC } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import { signOut, useLoggedInUser } from "./utils/firebase";
-import Button from "@material-ui/core/Button";
-import { AppBar, CircularProgress, Container, makeStyles, Toolbar } from "@material-ui/core";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { useLoggedInUser } from "./utils/firebase";
+import { CircularProgress, Container, makeStyles, Toolbar } from "@material-ui/core";
 import Home from "./pages/Home";
 import Notfound from "./pages/NotFound";
 import Login from "./pages/Login";
@@ -26,31 +24,6 @@ const App: FC = () => {
 
   return (
     <Router>
-      {/* <AppBar position='static'>
-        <Toolbar>
-          <Link className={classes.link} to='/'>
-            <Button className={classes.menuButton}>Home</Button>
-          </Link>
-          <Link className={classes.link} to='/map'>
-            <Button className={classes.menuButton}>Map</Button>
-          </Link>
-          {user && (
-            <>
-              <Link className={classes.link} to='/trips'>
-                <Button className={classes.menuButton}>Trips</Button>
-              </Link>
-              <Button className={classes.menuButton} onClick={signOut}>
-                Logout
-              </Button>
-            </>
-          )}
-          {user === null && (
-              <Link className={classes.link} to='/login'>
-                <Button className={classes.menuButton}>Login</Button>
-              </Link>
-          )}
-        </Toolbar>
-      </AppBar> */}
       <HeaderMenu user={user} />
 
       <main className="App">
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,8 +1,8 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import {useEffect, useState} from "react";
-import {Trip, User} from "./types";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { useEffect, useRef, useState } from "react";
+import { Trip, User } from "./types";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCD_r5I3BcheqHEeuCeZvzHrqD68R2vvSw",
@@ -10,35 +10,40 @@ const firebaseConfig = {
   projectId: "trip-planner-831cc",
   storageBucket: "trip-planner-831cc.appspot.com",
   messagingSenderId: "519013816293",
-  appId: "1:519013816293:web:46ff9be87160559fe5010c"
+  appId: "1:519013816293:web:46ff9be87160559fe5010c",
 };
 firebase.initializeApp(firebaseConfig);
-
 
 // Hook providing logged in user information
 export const useLoggedInUser = () => {
   // Hold user info in state
   const [user, setUser] = useState<firebase.User | null>();
 
+  const isMounted = useRef(false);
   // Setup onAuthStateChanged once when component is mounted
   useEffect(() => {
-    firebase.auth().onAuthStateChanged(u => setUser(u));
+    isMounted.current = true;
+    const unsubscribe = firebase.auth().onAuthStateChanged((u) => setUser(u));
+
+    // Cleanup of the hook
+    return () => {
+      isMounted.current = false;
+      unsubscribe();
+    };
   }, []);
 
   return user;
 };
 
 // Sign up handler
-export const signUp = (email: string, password: string) =>
-  firebase.auth().createUserWithEmailAndPassword(email, password);
+export const signUp = (email: string, password: string) => firebase.auth().createUserWithEmailAndPassword(email, password);
 
 // Sign in handler
-export const signIn = (email: string, password: string) =>
-  firebase.auth().signInWithEmailAndPassword(email, password);
+export const signIn = (email: string, password: string) => firebase.auth().signInWithEmailAndPassword(email, password);
 
 // Sign out handler
 export const signOut = () => firebase.auth().signOut();
 
 /********************************************* Firestore database ************************************************/
 const db = firebase.firestore();
-export const usersCollection = db.collection('users') as firebase.firestore.CollectionReference<User>;
+export const usersCollection = db.collection("users") as firebase.firestore.CollectionReference<User>;
